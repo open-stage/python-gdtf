8b96762b75eac12d2159cbfb682e9953c590f02f
vanous <petr@linuks.cz>
2023-09-22 13:12:26 +0200

    Updata data for 1.0.3 release

---
+++ CHANGELOG.md
@@ -0,0 +1,25 @@
+### Changelog
+
+#### Version 1.0.3
+* First release under Open-Stage organization
+    * add CI/CD pytest testing + mypy
+* Separating helper methods into utils
+    * support Geometry References
+    * complexity calculation
+    * support Virtual Channels
+* Additional supported objects
+    * DataVersion, MediaServerCamera, Inventory
+    * Wiring Object, DMX Break Overwrite, Geometry Reference
+    * MediaServerLayer, MediaServerMaster
+* Many additional improvements and fixes 
+    * more type hints
+    * default values to ColorCIE
+    * handle missing data (thumbnails, models)
+    * add extension to models
+    * added 1.1 PrimitiveTypes
+
+#### Version 1.0.2
+* Last release to pypi.org from the original author
+
+#### Version 0.1.0
+* First release to pypi.org
--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 
 setup(
     name="pygdtf",
-    version="1.0.1",
-    description="General Device Type Format library",
-    long_description="Basic library for interfacing with the General Device Type Format developed by MA, Robe and "
-    "Vectorworks. For more information on the format see gdtf-share.com",
+    version="1.0.3",
+    description="General Device Type Format library for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     license="MIT",
-    author="Jack Page",
-    author_email="jdpboc98@gmail.com",
+    author="Jack Page, vanous",
+    author_email="noreply@nodomain.com",
     packages=["pygdtf", "pygdtf.utils"],
+    url="https://github.com/open-stage/python-gdtf",
+    project_urls={
+        "Source": "https://github.com/open-stage/python-gdtf",
+        "Changelog": "https://github.com/open-stage/python-gdtf/blob/master/CHANGELOG.md",
+    },
 )
