6e5ed24c6127daed7e4e758a21721678a4c70650
vanous <petr@linuks.cz>
2025-07-22 13:39:41 +0200

    Add FTPresets implementation

--- pygdtf/__init__.py
+++ pygdtf/__init__.py
@@ -352,6 +352,14 @@ class FixtureType:
             for i in protocols_collect.findall("CITP"):
                 self.protocols.append(Citp(xml_node=i))
 
+        ft_presets_collect = self._root.find("FTPresets")
+        if ft_presets_collect is not None:
+            self.ft_presets = FTPresets(
+                [FTPreset(xml_node=i) for i in ft_presets_collect.findall("FTPreset")]
+            )
+        else:
+            self.ft_presets = FTPresets()
+
 
 class BaseNode:
     def __init__(
@@ -2476,3 +2484,59 @@ class OpenSoundControl(BaseNode):
 
 class Citp(BaseNode):
     pass
+
+class PresetValue(BaseNode):
+    def __init__(
+        self,
+        attribute: Optional["NodeLink"] = None,
+        value: "DmxValue" = DmxValue("0/1"),
+        *args,
+        **kwargs,
+    ):
+        self.attribute = attribute
+        self.value = value
+        super().__init__(*args, **kwargs)
+
+    def _read_xml(self, xml_node: "Element", xml_parent: Optional["Element"] = None):
+        self.attribute = NodeLink("Attributes", xml_node.attrib.get("Attribute"))
+        self.value = DmxValue(xml_node.attrib.get("Value", "0/1"))
+
+    def as_dict(self):
+        return {
+            "attribute": self.attribute.str_link,
+            "value": self.value.get_value(),
+        }
+
+
+class FTPreset(BaseNode):
+    def __init__(
+        self,
+        name: Optional[str] = None,
+        values: Optional[List["PresetValue"]] = None,
+        *args,
+        **kwargs,
+    ):
+        self.name = name
+        self.values = values if values is not None else []
+        super().__init__(*args, **kwargs)
+
+    def _read_xml(self, xml_node: "Element", xml_parent: Optional["Element"] = None):
+        self.name = xml_node.attrib.get("Name")
+        self.values = [PresetValue(xml_node=i) for i in xml_node.findall("Value")]
+
+    def as_dict(self):
+        return {
+            "name": self.name,
+            "values": [v.as_dict() for v in self.values],
+        }
+
+
+class FTPresets(list):
+    def get_preset_by_name(self, name):
+        for preset in self:
+            if preset.name == name:
+                return preset
+        return None
+
+    def as_dict(self):
+        return [p.as_dict() for p in self]
