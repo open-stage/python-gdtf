b43c2aea2d74abbe3d747b7f46ff3738beb9c338
vanous <petr@linuks.cz>
2026-02-01 19:25:26 +0100

    Release 1.4.4

--- pygdtf/__init__.py
+++ pygdtf/__init__.py
@@ -41,7 +41,7 @@ from .revisions import *
 from .utils import *
 from .value import *  # type: ignore
 
-__version__ = "1.4.4-dev5"
+__version__ = "1.4.4"
 
 # Standard predefined colour spaces: R, G, B, W-P
 COLOR_SPACE_SRGB = ColorSpaceDefinition(
